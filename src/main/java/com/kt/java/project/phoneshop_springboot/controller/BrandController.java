fd69e611005b17863922fed5d3b5671fd06e7762
KingTwenty20 <kt20@KingTwenty20s-MacBook-Pro.local>
2024-10-02 20:02:15 +0700

    home-new-service-implement

--- src/main/java/com/kt/java/project/phoneshop_springboot/controller/BrandController.java
+++ src/main/java/com/kt/java/project/phoneshop_springboot/controller/BrandController.java
@@ -2,6 +2,8 @@ package com.kt.java.project.phoneshop_springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,12 @@ public class BrandController {
 	public ResponseEntity<?> create(@RequestBody BrandDTO brandDTO) {
 		Brand brand = Mapper.toBrand(brandDTO);
 		brand = brandService.save(brand);
+		return ResponseEntity.ok(Mapper.toBrandDTO(brand));
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<?> getbrandById(@PathVariable("id") Integer brandId){
+		Brand brand = brandService.getById(brandId);
 		return ResponseEntity.ok(Mapper.toBrandDTO(brand));
 	}
 }
--- src/main/java/com/kt/java/project/phoneshop_springboot/serviceImpl/BrandServiceImpl.java
+++ src/main/java/com/kt/java/project/phoneshop_springboot/serviceImpl/BrandServiceImpl.java
@@ -1,7 +1,9 @@
 package com.kt.java.project.phoneshop_springboot.serviceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import com.kt.java.project.phoneshop_springboot.entity.Brand;
 import com.kt.java.project.phoneshop_springboot.repository.BrandRepository;
@@ -9,10 +11,10 @@ import com.kt.java.project.phoneshop_springboot.service.BrandService;
 
 @Service
 public class BrandServiceImpl implements BrandService {
-	
+
 	@Autowired
-	BrandRepository brandRepository;	
-	
+	BrandRepository brandRepository;
+
 	@Override
 	public Brand save(Brand brand) {
 		return brandRepository.save(brand);
@@ -20,10 +22,19 @@ public class BrandServiceImpl implements BrandService {
 
 	@Override
 	public Brand getById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+//		Optional<Brand> brandOptional = brandRepository.findById(id);
+//		if(brandOptional.isPresent()) {
+//			return brandOptional.get();
+//		} else
+//		{
+			//* Able to use format for java under 17
+//			throw new HttpClientErrorException(HttpStatus.NOT_FOUND,String.format("Brand with id %d is not found", id));
+			//* use this line for java 17 up
+			// throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "Brand with id %d is not found".formatted(id));
+//		}
+		return brandRepository.findById(id).orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND, 
+				"Brand with id %d is not found".formatted(id)
+				));
 	}
-	
-	
 
 }
