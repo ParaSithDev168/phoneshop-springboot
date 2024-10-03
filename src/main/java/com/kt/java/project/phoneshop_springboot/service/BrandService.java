960a2dabf36659fc5cbf4f409ac4a08290f35f36
KingTwenty20 <kt20@KingTwenty20s-MacBook-Pro.local>
2024-10-03 21:23:51 +0700

    MapStruct & BrandCRUD

--- src/main/java/com/kt/java/project/phoneshop_springboot/service/BrandService.java
+++ src/main/java/com/kt/java/project/phoneshop_springboot/service/BrandService.java
@@ -1,9 +1,14 @@
 package com.kt.java.project.phoneshop_springboot.service;
 
+import java.util.List;
+
 import com.kt.java.project.phoneshop_springboot.entity.Brand;
 
 public interface BrandService {
 	Brand save(Brand brand);
 	Brand getById(Integer id);
 	Brand updatebrand(Integer id, Brand brand);
+	List<Brand> getAllBrands();
+	List<Brand> getAllBrands(String name);
+	
 }
